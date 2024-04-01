d46c57e2e1efdc6c7318697deb510c1dbfdb2f5e
James Bradlee <j4m350n@testersen.no>
2024-04-01 09:57:50 +0200

    Update DependencyGroups.kt

--- buildSrc/src/main/kotlin/io/tnboot/gradle/build/DependencyGroups.kt
+++ buildSrc/src/main/kotlin/io/tnboot/gradle/build/DependencyGroups.kt
@@ -16,7 +16,7 @@ private val mapper = ObjectMapper(YAMLFactory())
 
 /**
  * A class that reads a YAML file containing dependency groups and
- * provides a list of dependencies and a map of version properties. test3
+ * provides a list of dependencies and a map of version properties. test4
  */
 class DependencyGroups(
 	file: File,
